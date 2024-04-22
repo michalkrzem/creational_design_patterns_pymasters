1f39b50e73e8d0e172ab6cc8b56b596f1e0df27a
mkrzeminski <mkrzeminski@imgw.pl>
2024-04-22 13:32:37 +0200

    Fabric by production, development, tests

--- config_manager.py
+++ config_manager.py
@@ -1,34 +1,63 @@
-class Configuration(object):
+from abc import ABC, abstractmethod
+
+
+class Configuration:
     _instance = None
-    _settings = {}
+    _app_settings = {}
 
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(Configuration, cls).__new__(cls)
         return cls._instance
 
-    def __init__(self):
-        self._dicter = {}
-
     @property
-    def dicter(self):
-        return self._dicter
+    def app_settings(self):
+        return self._app_settings
 
-    @dicter.setter
-    def dicter(self, **kwargs):
-        self.dicter.update(kwargs)
-
-
-a = Configuration()
-b = Configuration()
-
-b.dicter["ola"] = "kasia"
-print(b.dicter)
-
-a.dicter["kamila"] = "Ewa"
-print(a.dicter)
+    @app_settings.setter
+    def app_settings(self, **kwargs):
+        self._app_settings.update(kwargs)
 
 
-print(id(a))
-print(id(b))
-print(id(a) == id(b))
+class ConfigFactory(ABC):
+    @staticmethod
+    @abstractmethod
+    def create_config(key):
+        pass
+
+
+class DevelopConfig(ConfigFactory, Configuration):
+    @staticmethod
+    def create_config(key):
+        config = Configuration()
+        config.app_settings["develop"] = key
+        return config
+
+
+class ProductionConfig(ConfigFactory, Configuration):
+    @staticmethod
+    def create_config(key):
+        config = Configuration()
+        config.app_settings["production"] = key
+        return config
+
+
+class TestConfig(ConfigFactory, Configuration):
+    @staticmethod
+    def create_config(key):
+        config = Configuration()
+        config.app_settings["test"] = key
+        return config
+
+
+develop = DevelopConfig()
+production = ProductionConfig()
+test = TestConfig()
+
+develop.create_config({"Debug": True, "database": "Postgres"})
+production.create_config({"Debug": False, "database": "SQLite"})
+test.create_config({"Debug": None, "database": "MyDatabase"})
+
+print(develop.app_settings.get("production").get("Debug"))
+print(production.app_settings.get("production"))
+print(test.app_settings)
